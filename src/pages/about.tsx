69097bda634429be3f77703938e6dbd5c027d39c
Jeet Singh <taranjeet0007@gmail.com>
2024-09-17 00:15:35 +0700

    fix responsiveness

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,23 +1,21 @@
 
 const About = () => {
     return (
-        <div className="flex flex-row" id="about">
-            <div className="basis-1/2">
-                <section className="mb-16">
-                    <h3 className="text-2xl font-bold mb-4">About Me</h3>
-                    <p className="text-gray-700 mb-4">
-                        Experienced Software Developer adept in bringing forth expertise in design,
-                        installation, testing and maintenance of software systems. Equipped with a diverse and
-                        promising skill-set. Proficient in various platforms, languages, and embedded systems. Experienced with the latest cutting
-                        edge development tools and procedures. Able to effectively self-manage during independent projects, as well as collaborate as part of a productive team.
-                    </p>
-                    <p className="text-gray-700">
-                        Great relationship with leadership and clients. As was appointed for Technical
-                        Account Officer role within the 2nd year at my job.
-                    </p>
-                </section>
-            </div>
-        </div>
+
+        <section className="mb-16">
+            <h3 className="text-2xl font-bold mb-4">About Me</h3>
+            <p className="text-gray-700 mb-4">
+                Experienced Software Developer adept in bringing forth expertise in design,
+                installation, testing and maintenance of software systems. Equipped with a diverse and
+                promising skill-set. Proficient in various platforms, languages, and embedded systems. Experienced with the latest cutting
+                edge development tools and procedures. Able to effectively self-manage during independent projects, as well as collaborate as part of a productive team.
+            </p>
+            <p className="text-gray-700">
+                Great relationship with leadership and clients. As was appointed for Technical
+                Account Officer role within the 2nd year at my job.
+            </p>
+        </section>
+
     )
 }
 
